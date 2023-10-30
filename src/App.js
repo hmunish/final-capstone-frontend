a7a84d4092895621c33ed90a6acb4972df8f7c9c
Sunga Thawethe <sungabt@gmail.com>
2023-10-30 18:33:16 +0200

    Add Nav Menu component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Cars from './components/Cars';
 import Main from './components/Main';
+import NavMenu from './components/NavMenu';
 
 function App() {
   return (
     <main>
+      <NavMenu />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Cars" element={<Cars />} />
---
+++ src/components/NavMenu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '../CSS/NavMenu.module.css';
+
+const NavMenu = () => (
+  <header>
+    <img alt="Logo" />
+    <nav className={styles.navigation}>
+      <ul className={styles.navlist}>
+        <li>
+          <NavLink to="/cars">Cars</NavLink>
+        </li>
+        <li>
+          <NavLink to="/reservations">Reservations</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cars/new">New Car</NavLink>
+        </li>
+        <li>
+          <NavLink to="/reservations/new">New Reservation</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default NavMenu;
