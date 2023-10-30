128bac85bdf79970a925c637e03cfc79c622b9b5
Sunga Thawethe <sungabt@gmail.com>
2023-10-30 22:46:23 +0200

    Add some styling for navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Cars from './components/Cars';
 import Main from './components/Main';
 import NavMenu from './components/NavMenu';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <main>
-      <NavMenu />
+      <button onClick={toggleMenu} type="button">Menu</button>
+      {isOpen && <NavMenu />}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Cars" element={<Cars />} />
