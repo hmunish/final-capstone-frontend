cad70973e036a2a379ab424d3d5a1ed03f68a3c3
Sunga Thawethe <sungabt@gmail.com>
2023-10-30 13:12:54 +0200

    Add carsReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import carsReducer from './cars/carsSlice';
 
-const store = configureStore({ reducer: {} });
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+  },
+});
 
 export default store;
