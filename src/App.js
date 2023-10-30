abc7c904f7c2ea330943dd475c2ccfd7fd6cc4b6
Munish <hmunish5@gmail.com>
2023-10-30 11:37:39 +0200

    Add jwt-decode node package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import { Routes } from 'react-router-dom';
+import Login from './components/login/login';
 
 function App() {
+  // Checking if the authentication token key exists
+  const authKey = localStorage.getItem('authKey');
+  if (!authKey) {
+    return <Login />;
+  }
   return (
     <main>
       <Routes />
---
+++ src/redux/login/loginSlice.js
@@ -0,0 +1,3 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const API_URL = "http://localhost:3000/api/v1/login";
